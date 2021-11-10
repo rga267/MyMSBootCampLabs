5d76b0b8217d0c5a4e4a10e2c9c01e7d3cb11e19
Ricardo <rga267@nyu.edu>
2021-11-10 09:49:07 -0500

    lab 4 implemnetation

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ console.log("It's ALive!");
 
 var Player = require('./jasmine_examples/Player');
 var Song = require('./jasmine_examples/Song');
-var GoogleBook = require('./.git')
+var GoogleBook = require('./dist/GoogleBook')
+var googleBookSvc = require ('./dist/GoogleBookService.js');
+const googleBookItemCatStub = require('./GoogleBookItem');
+
 
 var song = new Song();
 var player = new Player();
+var googlebook = new GoogleBook();
+googlebook.googleBookItemMapper(googleBookItemCatStub);
 player.play(song);
 player.pause();
 player.resume();
-player.makeFavorite();
 
+const service = new googleBookSvc.GoogleBookService();
+service.fetchBooks("Cats");
+setTimeout(function(){ console.log(service.fetchedResults); }, 1000);
+
+module.exports = {googleBookSvc, GoogleBook}
